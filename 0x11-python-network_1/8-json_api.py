0c3ab5574f6caf9d143c419caa5f305c0fdaa2e2
Ashraf <achraf.rassili98@gmail.com>
2023-09-29 09:10:38 -0700

    tasks done

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+    script that takes in a letter and sends a POST
+    request to http://0.0.0.0:5000/search_user with
+    the letter as a parameter.
+'''
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    my_dict = {"q": sys.argv[1] if len(sys.argv) > 1 else ""}
+    req = requests.post("http://0.0.0.0:5000/search_user", data=my_dict)
+    try:
+        j_dict = req.json()
+        if j_dict:
+            print("[{}] {}". format(j_dict.get("id"), j_dict.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
