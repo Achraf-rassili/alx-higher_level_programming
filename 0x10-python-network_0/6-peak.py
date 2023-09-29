c914d4a6dccc033b80b3a9fdd19dbaf04e4d36ad
Ashraf <achraf.rassili98@gmail.com>
2023-09-28 19:15:58 -0700

    task 7 correction

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,33 @@
 #!/usr/bin/python3
-""" Test function find_peak"""
-find_peak = __import__('6-peak').find_peak
+'''finds a peak in a list of integers'''
 
-print(find_peak([1, 2, 4, 6, 3]))
-# print(find_peak([4, 1, 2, 2, 3, 1]))
-# print(find_peak([2, 2, 2]))
-# print(find_peak([]))
-# print(find_peak([-2, -4, 2, 1]))
-# print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
+
+def find_peak(list_of_integers):
+    '''function tto find the peak'''
+    if len(list_of_integers) == 0:
+        return None
+
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    mid = (len(list_of_integers) - 1) // 2
+
+    if mid == 0:
+        if list_of_integers[mid] >= list_of_integers[mid + 1]:
+            return list_of_integers[mid]
+        else:
+            return list_of_integers[mid + 1]
+
+    if mid == (len(list_of_integers) - 1):
+        if list_of_integers[mid] >= list_of_integers[mid - 1]:
+            return list_of_integers[mid]
+        else:
+            return list_of_integers[mid - 1]
+
+    left = find_peak(list_of_integers[:mid])
+    right = find_peak(list_of_integers[mid:])
+
+    if left < right:
+        return right
+    else:
+        return left
